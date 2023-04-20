69b07a378bab801b8d2941dd873fa7200f328e89
Ryan Du Plooy <ryanduplooy015@gmail.com>
2023-04-20 04:59:20 +0000

    Initial commit for circleci setup

--- spec/controller/project_functional_spec.rb
+++ spec/controller/project_functional_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 RSpec.describe ProjectsController, :type => :controller do
+    # Add this
+    login_user
+
+    let(:valid_attributes) {
+        { :title => "Test title!", :description => "This is a test description"}
+    }
+    
+    let(:valid_session) { {} }
+
     describe "test web requests" do
         context "GET #index" do
             it "returns a success response" do
@@ -21,6 +30,13 @@ RSpec.describe ProjectsController, :type => :controller do
         it "returns a success response" do
             delete :index
             expect(response).to have_http_status(:ok)
+        end
+
+        it "returns a success response" do
+            Project.create! valid_attributes
+            get :index, params: {}, session: valid_session
+            expect(response).to be_successful # be_successful expects a HTTP Status code of 200
+            # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
         end
     end
 
--- spec/features/project_features_spec.rb
+++ spec/features/project_features_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.feature "Projects", type: :feature do
     context "Create project" do
         before(:each) do
+          user = FactoryBot.create(:user)
+          login_as(user)
           visit new_project_path
         end
 
@@ -34,6 +36,8 @@ RSpec.feature "Projects", type: :feature do
     context "Update project" do
       let(:project) { Project.create(title: "Test title", description: "Test content") }
       before(:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
         visit edit_project_path(project)
       end
  
@@ -62,6 +66,28 @@ RSpec.feature "Projects", type: :feature do
         expect(page).to have_content("Description can't be blank")
       end
     end
+    context "Login" do
+      scenario "should sign up" do
+        visit root_path
+        click_link 'Sign up'
+        within("form") do
+          fill_in "Email", with: "testing@test.com"
+          fill_in "Password", with: "123456"
+          fill_in "Password confirmation", with: "123456"
+          click_button "Sign up"
+        end
+        expect(page).to have_content("Welcome! You have signed up successfully.")
+      end
+  
+  
+      scenario "should log in" do
+        user = FactoryBot.create(:user)
+        login_as(user)
+        visit root_path
+        expect(page).to have_content("Logged in")
+      end
+    end
+  
 end
  
 
